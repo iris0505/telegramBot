486d33013998f7f89b304d073d9cc67ffceb55b9
iris0505 <165427618+iris0505@users.noreply.github.com>
2024-04-08 20:15:52 +0300

    Add files via upload

--- FullName.cs
+++ FullName.cs
@@ -1,9 +1,6 @@
 namespace Domain.ValueObject;
 
 public class FullName : BaseValueObject {
-    public FullName(string firstName, string lastName, string middleName) {
-        var validator
-    }
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
     public string MiddleName { get; set; }
